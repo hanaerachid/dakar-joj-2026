12a18b4f7391e666ca1d945a4925a4fd4959a0f1
Youssef <youssefallalix@gmail.com>
2026-09-09 23:30:29 +0000

    fix: update INITIAL_ZOOM value to correct map zoom level

--- src/utils/mapConfig.ts
+++ src/utils/mapConfig.ts
@@ -1,5 +1,5 @@
 export const INITIAL_CENTER: [number, number] = [-17.194, 14.583];
-export const INITIAL_ZOOM = 10.12;
+export const INITIAL_ZOOM = 9.5;
 export const MOBILE_INITIAL_ZOOM = 8.2;
 
 export const getInitialZoom = (): number => {
